6f31c8a5ddbf9ac3e6be9484e68ef560c6ce155c
reillydonovan <reillydonovan3@gmail.com>
2017-10-08 00:34:03 -0700

    Revert "got the supershape to work"
    This reverts commit f5aa72d4425dd0e14ef70c7acac9950c09540981.

--- SuperShapes/Assets/Scripts/Sphere.cs
+++ SuperShapes/Assets/Scripts/Sphere.cs
@@ -15,7 +15,12 @@ public class Sphere : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
         this.UpdateMesh(GetComponent<MeshFilter>().mesh);
+
+
+        
+
 	}
 
     Mesh UpdateMesh(Mesh m)
@@ -28,8 +33,8 @@ public class Sphere : MonoBehaviour {
         //clear out the old mesh
         m.Clear();
 
-        Vector3[] vectors = new Vector3[(resolution + 1) * (resolution)];
-        Vector2[] uvs = new Vector2[(resolution + 1) * (resolution)];
+        Vector3[] vectors = new Vector3[(resolution + 1) * (resolution )];
+        Vector2[] uvs = new Vector2[(resolution + 1) * (resolution )];
 
         float seconds = Time.timeSinceLevelLoad;
 
